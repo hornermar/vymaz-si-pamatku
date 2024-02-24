23b7ea98155b0cb1b4c3b67b3e73fbd2a59cb3e7
hornermar <hornerova.m@gmail.com>
2024-02-24 11:30:39 +0100

    add zeleznicni most

---
+++ confetti.js
@@ -0,0 +1,54 @@
+export function createConfetti() {
+    const confettiCount = 200;
+    const confettiColors = [
+        "#f5df4d",
+        "#d2c800",
+        "#c0c4c8",
+        "#939597",
+        "black",
+    ];
+
+    for (let i = 0; i < confettiCount; i++) {
+        let confetti = document.createElement("div");
+        confetti.classList.add("confetti");
+        confetti.style.backgroundColor =
+            confettiColors[Math.floor(Math.random() * confettiColors.length)];
+        confetti.style.position = "fixed";
+        confetti.style.height = "10px";
+        confetti.style.width = "10px";
+        confetti.style.top = Math.random() * window.innerHeight + "px";
+        confetti.style.left = Math.random() * window.innerWidth + "px";
+        confetti.style.zIndex = 9999;
+        document.body.appendChild(confetti);
+
+        animateConfetti(confetti);
+    }
+}
+
+const animateConfetti = (confetti) => {
+    let initialX = Math.random() * 10 - 5;
+    let initialY = Math.random() * 10 - 5;
+    let rotation = Math.random() * 360;
+
+    let transform = `translate(${initialX}px, ${initialY}px) rotate(${rotation}deg)`;
+    confetti.style.transform = transform;
+
+    setInterval(() => {
+        confetti.style.top = confetti.offsetTop + 8 + "px";
+        confetti.style.transform = `translate(${initialX}px, ${initialY}px) rotate(${rotation}deg)`;
+
+        if (confetti.offsetTop > window.innerHeight) {
+            confetti.style.top = 0;
+        }
+        if (confetti.offsetLeft > window.innerWidth) {
+            confetti.style.left = 0;
+        }
+    }, 1000 / 60);
+};
+
+export const removeConfetti = () => {
+    const confetti = document.querySelectorAll(".confetti");
+    confetti.forEach((c) => {
+        document.body.removeChild(c);
+    });
+};
