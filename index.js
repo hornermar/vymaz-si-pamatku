65d7ec5ff2972b2148de2790bbe9e274ce41055a
hornermar <hornerova.m@gmail.com>
2024-03-02 12:14:46 +0100

    add pixel buldozer

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const isMobile = window.matchMedia(
     "only screen and (max-width: 760px)"
 ).matches;
 
-let src = memorials[0].src;
+let src = memorials[1].src;
 const img = new Image();
 img.src = src;
 
@@ -24,16 +24,15 @@ const changeMemorial = (e) => {
     const src = memorials.find((m) => m.value === e.target.value).src;
     img.src = src;
     img.onload();
+    isWon = false;
 };
 changeMemorialButtons.forEach((btn) => {
     btn.addEventListener("click", changeMemorial);
 });
 
-const excavator = document.getElementById("excavator");
+const bulldozer = document.getElementById("bulldozer");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-
-// const counter = document.getElementById("counter");
 
 const clearRectSize = isMobile ? 20 : 40;
 
@@ -52,11 +51,11 @@ canvas.height = height;
 img.onload = function () {
     ctx.drawImage(img, 0, 0, width, height);
 
-    // move excavator to the center of the canvas
+    // move bulldozer to the center of the canvas
     const canvasPosition = canvas.getBoundingClientRect();
-    const canvasX = canvasPosition.left + width - 5;
+    const canvasX = canvasPosition.left + width - 45;
     const canvasY = canvasPosition.top + height - 20;
-    excavator.style = `--top: ${canvasY}px; --left: ${canvasX}px;`;
+    bulldozer.style = `--top: ${canvasY}px; --left: ${canvasX}px;`;
 };
 
 // Calculate transparency
@@ -79,45 +78,8 @@ const getEmptyPixelsRatio = () => {
 
     const result = alphaValues.length / maxPixels;
 
-    // counter.innerHTML = result * 100 + " %";
     return result;
 };
-
-// const displayNotDestroyed = () => {
-//     const imageData = ctx.getImageData(0, 0, size, size).data;
-//     // assuming imageData is an array of pixel data
-//     for (let i = 0; i < imageData.length; i += 4) {
-//         // if the color of a pixel is not transparent or white, set it to the red color
-//         if (
-//             !(
-//                 (
-//                     (imageData[i] === 255 &&
-//                         imageData[i + 1] === 255 &&
-//                         imageData[i + 2] === 255) || // white
-//                     imageData[i + 3] === 0
-//                 ) // transparent
-//             )
-//         ) {
-//             imageData[i] = 255; // Red
-//             imageData[i + 1] = 0; // Green
-//             imageData[i + 2] = 0; // Blue
-//             imageData[i + 3] = 255; // Alpha
-//         }
-//     }
-//     // Put the modified image data back into the canvas
-//     ctx.putImageData(
-//         new ImageData(
-//             new Uint8ClampedArray(imageData),
-//             canvas.width,
-//             canvas.height
-//         ),
-//         0,
-//         0
-//     );
-// };
-
-// const notDestroyedBtn = document.getElementById("not-destroyed");
-// notDestroyedBtn.addEventListener("click", displayNotDestroyed);
 
 const move = (mouse) => {
     if (!isPress) return;
@@ -130,8 +92,8 @@ const move = (mouse) => {
     const canvasY = clientY - canvasPosition.top;
 
     if (canvasX > 0 && canvasX < width && canvasY > 0 && canvasY < height) {
-        // Move excavator
-        excavator.style = `--top: ${clientY}px; --left: ${clientX}px;`;
+        // Move bulldozer
+        bulldozer.style = `--top: ${clientY}px; --left: ${clientX}px;`;
 
         // Clear
         ctx.clearRect(
@@ -144,7 +106,6 @@ const move = (mouse) => {
         if (isWon) return;
 
         // Check if canvas is empty
-
         if (getEmptyPixelsRatio() >= 0.996) {
             isWon = true;
             isConfetti = true;
