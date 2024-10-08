78e4f122f0cbee5a9e679f3d376c76de95444da7
hornermar <hornerova.m@gmail.com>
2024-10-08 14:29:58 +0200

    fix description, add percentage

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { createConfetti, removeConfetti } from "./confetti.js";
 
+// State variables
 let isPress = false;
 let isWon = false;
 let isConfetti = false;
@@ -7,82 +8,13 @@ let intervalId;
 let wasPressed = false;
 let initialEmptyPixelRatio = 0;
 
+// Utility functions
 function getRandomIndex(array) {
-  const randomIndex = Math.floor(Math.random() * array.length);
-  return randomIndex;
+  return Math.floor(Math.random() * array.length);
 }
 
-function updateProgressBar(percentage) {
-  const progressBar = document.getElementById("progress-bar");
-  let newPercentage = percentage;
-
-  if (percentage !== 0 & percentage !== 100) {
-    const initialEmptyPercentage = initialEmptyPixelRatio * 100;
-
-    // Calculate the effective range for the progress bar
-    const effectiveRange = 100 - initialEmptyPercentage;
-    // Adjust the given percentage to account for the initial empty space
-    const adjustedPercentage = percentage - initialEmptyPercentage;
-    newPercentage = (adjustedPercentage / effectiveRange) * 100;
-  }
-
-  // Set the width of the progress bar
-  progressBar.style.width = newPercentage + "%";
-}
-
-const memorials = [
-  { src: "./zeleznicni-most.png", value: "zeleznicniMost" },
-  { src: "./chemapol.png", value: "chemapol" },
-];
-
-const isMobile = window.matchMedia(
-  "only screen and (max-width: 760px)"
-).matches;
-
-const randomIndex = getRandomIndex(memorials);
-let src = memorials[randomIndex].src;
-const img = new Image();
-img.src = src;
-
-const endDescriptionEl = document.getElementById("end-description");
-
-const changeMemorialButtons = document.querySelectorAll("#change-btn");
-const changeMemorial = (e) => {
-  endDescriptionEl.style.display = "none";
-  const src = memorials.find((m) => m.value === e.target.value).src;
-  img.src = src;
-  img.onload();
-  isWon = false;
-  updateProgressBar(0);
-};
-changeMemorialButtons.forEach((btn) => {
-  btn.addEventListener("click", changeMemorial);
-});
-
-const bulldozer = document.getElementById("bulldozer");
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d", { willReadFrequently: true });
-
-const clearRectSize = isMobile ? 20 : 40;
-
-// calculate the size of the canvas depending of the size of the window
-const windowWidth = window.innerWidth;
-const windowHeight = isMobile ? (window.innerHeight / 100) * 45 : window.innerHeight;
-const calculatedSize = Math.min(windowWidth, windowHeight) * 0.9;
-const size = calculatedSize < 500 ? calculatedSize : 500;
-
-const width = size;
-const height = (3 / 4) * size;
-
-canvas.width = width;
-canvas.height = height;
-
-// Calculate transparency
-const maxPixels = width * height;
-
-const getEmptyPixelsRatio = () => {
+function getEmptyPixelsRatio() {
   const imageData = ctx.getImageData(0, 0, width, height).data;
-
   let alphaValues = [];
   for (let i = 0; i < imageData.length; i += 4) {
     if (
@@ -94,39 +26,40 @@ const getEmptyPixelsRatio = () => {
       alphaValues.push(i);
     }
   }
+  return alphaValues.length / maxPixels;
+}
 
-  const result = alphaValues.length / maxPixels;
+function updateProgressBar(percentage) {
+  let newPercentage = percentage;
+  if (percentage !== 0) {
+    const initialEmptyPercentage = initialEmptyPixelRatio * 100;
+    const effectiveRange = 100 - initialEmptyPercentage;
+    const adjustedPercentage = percentage - initialEmptyPercentage;
+    newPercentage = (adjustedPercentage / effectiveRange) * 100;
+  }
+  progressBar.style.width = newPercentage + "%";
+}
 
-  return result;
-};
+// Event handlers
+function changeMemorial(e) {
+  endDescriptionEl.style.display = "none";
+  const src = memorials.find((m) => m.value === e.target.value).src;
+  img.src = src;
+  img.onload();
+  isWon = false;
+  updateProgressBar(0);
+}
 
-img.onload = function () {
-  ctx.drawImage(img, 0, 0, width, height);
-
-  // move bulldozer to the center of the canvas
-  const canvasPosition = canvas.getBoundingClientRect();
-  const canvasX = canvasPosition.left + width - 40;
-  const canvasY = canvasPosition.top + height - 20;
-  bulldozer.style = `--top: ${canvasY}px; --left: ${canvasX}px;`;
-
-  initialEmptyPixelRatio = getEmptyPixelsRatio();
-};
-
-const move = (mouse) => {
+function move(mouse) {
   if (!isPress) return;
-
   const clientX = mouse.clientX ? mouse.clientX : mouse.touches[0].clientX;
   const clientY = mouse.clientY ? mouse.clientY : mouse.touches[0].clientY;
-
   const canvasPosition = canvas.getBoundingClientRect();
   const canvasX = clientX - canvasPosition.left;
   const canvasY = clientY - canvasPosition.top;
 
   if (canvasX > 0 && canvasX < width && canvasY > 0 && canvasY < height) {
-    // Move bulldozer
     bulldozer.style = `--top: ${clientY}px; --left: ${clientX}px;`;
-
-    // Clear
     ctx.clearRect(
       canvasX - clearRectSize / 2,
       canvasY - clearRectSize / 2,
@@ -137,10 +70,12 @@ const move = (mouse) => {
     if (isWon) return;
 
     const emptyPixelRation = getEmptyPixelsRatio();
+    progressBarPercentage.textContent = `${(emptyPixelRation * 100).toFixed(
+      2
+    )}%`;
     updateProgressBar(emptyPixelRation * 100);
 
-    // Check if canvas is empty
-    if (emptyPixelRation >= 0.996) {
+    if (emptyPixelRation >= 0.995) {
       isWon = true;
       isConfetti = true;
       createConfetti();
@@ -148,30 +83,96 @@ const move = (mouse) => {
       updateProgressBar(100);
     }
   }
-};
+}
 
-const pressCanvas = () => {
+function moveBulldozer(event) {
+  const clientX = event.clientX ? event.clientX : event.touches[0].clientX;
+  const clientY = event.clientY ? event.clientY : event.touches[0].clientY;
+  bulldozer.style = `--top: ${clientY}px; --left: ${clientX}px;`;
+}
+
+function pressCanvas(event) {
   isPress = true;
-
+  moveBulldozer(event);
   if (!wasPressed) {
     wasPressed = true;
-
-    const pressDescriptionEl = document.getElementById("press-description");
-    pressDescriptionEl.style.color = "white";
+    pressDescriptionEl.style.display = "none";
   }
-};
+}
 
-const releaseCanvas = () => {
+function releaseCanvas() {
   isPress = false;
-};
+}
 
-const press = () => {
+function press() {
   if (isConfetti) {
     isConfetti = false;
     clearInterval(intervalId);
     removeConfetti();
   }
+}
+
+function handlePressDescriptionClick(event) {
+  pressDescriptionEl.style.display = "none";
+  moveBulldozer(event);
+}
+
+function pressProgressBar() {
+  progressBarPercentage.style.color = "black";
+}
+
+// Initialization
+const memorials = [
+  { src: "./zeleznicni-most.png", value: "zeleznicniMost" },
+  { src: "./chemapol.png", value: "chemapol" },
+];
+
+const isMobile = window.matchMedia(
+  "only screen and (max-width: 760px)"
+).matches;
+const randomIndex = getRandomIndex(memorials);
+let src = memorials[randomIndex].src;
+const img = new Image();
+img.src = src;
+
+const pressDescriptionEl = document.getElementById("press-description");
+const endDescriptionEl = document.getElementById("end-description");
+const changeMemorialButtons = document.querySelectorAll("#change-btn");
+const progressBar = document.getElementById("progress-bar");
+const progressBarPercentage = document.getElementById(
+  "progress-bar__percentage"
+);
+const bulldozer = document.getElementById("bulldozer");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d", { willReadFrequently: true });
+
+const clearRectSize = isMobile ? 20 : 40;
+const windowWidth = window.innerWidth;
+const windowHeight = isMobile
+  ? (window.innerHeight / 100) * 45
+  : window.innerHeight;
+const calculatedSize = Math.min(windowWidth, windowHeight) * 0.9;
+const size = calculatedSize < 500 ? calculatedSize : 500;
+const width = size;
+const height = (3 / 4) * size;
+
+canvas.width = width;
+canvas.height = height;
+const maxPixels = width * height;
+
+img.onload = function () {
+  ctx.drawImage(img, 0, 0, width, height);
+  const canvasPosition = canvas.getBoundingClientRect();
+  const canvasX = canvasPosition.left + width - 40;
+  const canvasY = canvasPosition.top + height - 20;
+  bulldozer.style = `--top: ${canvasY}px; --left: ${canvasX}px;`;
+  initialEmptyPixelRatio = getEmptyPixelsRatio();
 };
+
+// Event listeners
+changeMemorialButtons.forEach((btn) => {
+  btn.addEventListener("click", changeMemorial);
+});
 
 canvas.addEventListener("mousedown", pressCanvas);
 canvas.addEventListener("mouseup", releaseCanvas);
@@ -183,3 +184,6 @@ window.addEventListener("touchmove", move);
 
 window.addEventListener("mousedown", press);
 window.addEventListener("touchstart", press);
+
+pressDescriptionEl.addEventListener("click", handlePressDescriptionClick);
+progressBar.addEventListener("click", pressProgressBar);
